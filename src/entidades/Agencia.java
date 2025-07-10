04ebf0b3eec2f8ce96abeb9b9a1057f1d7c50807
LOQ <LOQ@Syel>
2025-07-09 22:37:17 -0600

    Creando la clase agencia

---
+++ src/entidades/Agencia.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author LOQ
+ */
+public class Agencia {
+    private String IDAgengia;
+    
+    
+}
