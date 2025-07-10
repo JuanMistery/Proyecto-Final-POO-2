84a039e28d141139405733041cea7e470413afdd
LOQ <LOQ@Syel>
2025-07-09 23:03:51 -0600

    Modifique Agencia y Empleado, y agregue Administrador y EmpleadoAgencia

--- src/entidades/Agencia.java
+++ src/entidades/Agencia.java
@@ -10,6 +10,43 @@ package entidades;
  */
 public class Agencia {
     private String IDAgengia;
+    private String nombreAgencia;
+    private String direccion;
+
+    public Agencia(String IDAgengia, String nombreAgencia, String direccion) {
+        this.IDAgengia = IDAgengia;
+        this.nombreAgencia = nombreAgencia;
+        this.direccion = direccion;
+    }
+
+    public String getIDAgengia() {
+        return IDAgengia;
+    }
+
+    public void setIDAgengia(String IDAgengia) {
+        this.IDAgengia = IDAgengia;
+    }
+
+    public String getNombreAgencia() {
+        return nombreAgencia;
+    }
+
+    public void setNombreAgencia(String nombreAgencia) {
+        this.nombreAgencia = nombreAgencia;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Agencia{" + "IDAgengia=" + IDAgengia + ", nombreAgencia=" + nombreAgencia + ", direccion=" + direccion + '}';
+    }
     
     
 }
--- src/entidades/Empleado.java
+++ src/entidades/Empleado.java
@@ -9,5 +9,51 @@ package entidades;
  * @author LOQ
  */
 public class Empleado extends Persona {
+    private String usuario;
+    private String contrasenia;
+    private String tipoEmpleado;
+
+    public Empleado(String usuario, String contrasenia, String tipoEmpleado, String nombres, String apellidos, String telefono, String direccion) {
+        super(nombres, apellidos, telefono, direccion);
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public Empleado(String usuario, String contrasenia, String tipoEmpleado) {
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getTipoEmpleado() {
+        return tipoEmpleado;
+    }
+
+    public void setTipoEmpleado(String tipoEmpleado) {
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "usuario=" + usuario + ", contrasenia=" + contrasenia + ", tipoEmpleado=" + tipoEmpleado + '}';
+    }
+    
     
 }
